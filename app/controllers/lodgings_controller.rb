039473ff04b7d7159923d5bb221a9a70c8d5a0cd
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2023-03-13 11:19:47 -0500

    fill out lodging controller

--- app/controllers/lodgings_controller.rb
+++ app/controllers/lodgings_controller.rb
@@ -1,2 +1,33 @@
 class LodgingsController < ApplicationController
+    def index
+        render json: Lodging.all
+    end
+
+    def show 
+        lodging = Lodging.find(params[:id])
+        render json: lodging, status: :ok
+    end
+
+    def create
+        lodging = Lodging.create!(lodging_params)
+        render json: lodging, status: :created
+    end
+
+    def update
+        lodging = Lodging.find(params[:id])
+        lodging.update!(lodging_params)
+        render json:lodging, status: :accepted
+    end
+
+    def destroy
+        lodging = Lodging.find(params[:id])
+        lodging.destroy
+        head :no_content
+    end
+
+    private
+
+    def lodging_params
+        params.permit(:link, :image, :season, :guests, :dates)
+    end
 end
