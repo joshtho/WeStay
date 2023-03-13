7dc69a49ec1ec61ad5c2449e896cd0697400b26e
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2023-03-13 11:14:22 -0500

    fill out location controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,33 @@
 class LocationsController < ApplicationController
+    def index
+        render json: Location.all
+    end
+
+    def show 
+        location = Location.find(params[:id])
+        render json: location, status: :ok
+    end
+
+    def create
+        location = Location.create!(location_params)
+        render json: location, status: :created
+    end
+
+    def update
+        location = Location.find(params[:id])
+        location.update!(location_params)
+        render json: location, status: :accepted
+    end
+
+    def destroy
+        location = Location.find(params[:id])
+        location.destroy
+        head :no_content
+    end
+
+    private
+
+    def location_params
+        params.permit(:name, :image, :description)
+    end
 end
