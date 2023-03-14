3d9758ba74602baea9067892c7ebc7abb07d5231
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2023-03-14 10:44:03 -0500

    fill out LocationList, LocationTile, and LocationPage, get routes working

---
+++ client/src/components/locations/LocationPage.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {useParams, Link} from 'react-router-dom'
+import LodgingTile from '../lodging/LodgingTile'
+import Button from 'react-bootstrap/Button'
+
+function LocationPage({lodgings, locations, onHandleDelete}) {
+const params = useParams()
+const locationId = parseInt(params.id)
+const currentLocation = locations.find(location => location.id === locationId)
+const stays = lodgings.filter(lodging => locationId === lodging.location_id)
+const listStays = () => (stays.map(stay => (
+    <LodgingTile 
+    key={stay.id} 
+    stay={stay} 
+    location={currentLocation} 
+    onHandleDelete={onHandleDelete} 
+    />
+)))
+console.log(lodgings)
+return (
+    <div>
+        Lodgings
+        {/* {currentLocation ? 
+        <div>
+            <h1>{currentLocation.name}</h1>
+            {listStays()}
+            <Link to={`/lodgings/${locationId}`} >
+                <Button>Add new lodging</Button>
+            </Link>
+            
+        </div>
+         : "" } */}
+    </div>
+    )
+}
+      
+export default LocationPage
---
+++ client/src/components/locations/LocationTile.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import {Link} from 'react-router-dom'
+
+function LocationTile({location}) {
+
+  return (
+    <Card style={{ width: '18rem' }} className='column' >
+      <Card.Img variant="top" src={location.image} />
+      <Card.Body>
+        <Card.Title>{location.name}</Card.Title>
+        <Card.Text>{location.description}</Card.Text>
+        <Link to={`${location.id}`}>
+            <Button>Your stays</Button>
+        </Link>
+        <Link to={`${location.id}/edit`}>
+            <Button>Edit</Button>
+        </Link>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default LocationTile
