92a25fe9d2316d053f0e151f5b797b0e4250b84e
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2023-08-28 16:17:47 -0500

    change site name

--- client/src/components/static/HomePage.jsx
+++ client/src/components/static/HomePage.jsx
@@ -6,9 +6,9 @@ function HomePage({loading}) {
   }
   return (
     <div>
-    <h1>Plan your stay</h1>
+    <h1>WeStay</h1>
       <img alt='groupPhoto' src='https://static01.nyt.com/images/2019/07/14/travel/14TIPS/63f2d864d66c4a119b036e8d4c3a5978-articleLarge.jpg?quality=75&auto=webp&disable=upscale' />
-      <p>Plan your stay is a website that allows you to save vacation ideas 
+      <p>WeStay is a website that allows you to save vacation ideas 
         with a link to the lodging you have your eye on.
       </p>
     </div>
