9f71d758f9cc1d639deb08498db2be10c0b27d6c
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2023-09-07 14:36:37 -0500

    update NavBar, update favicon

--- client/src/components/navigation/NavBar.jsx
+++ client/src/components/navigation/NavBar.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Dropdown from 'react-bootstrap/Dropdown'
+import Modal from 'react-bootstrap/Modal'
 import {Link, useNavigate} from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
 
-function NavBar({loggedIn, user, setUser, setLoggedIn}) {
+function NavBar({user, setUser, setLoggedIn}) {
     const navigate = useNavigate()
-    
+    const [showModal, setShowModal] = useState(false);
+
     function handleLogout() {
         fetch("/logout", { method: "DELETE" }).then((r) => {
             if (r.ok) {
             setLoggedIn(false)
+            setShowModal(false)
             navigate("/")
             setUser(null);
             }
@@ -17,50 +23,66 @@ function NavBar({loggedIn, user, setUser, setLoggedIn}) {
     }
   return (
     <>
-        {
-            loggedIn ? 
-        <Nav className="justify-content-center" activeKey="/">
-            <Nav.Item>
-                <Link to="/">
-                    <Button>Home</Button>
-                </Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Link to="/lodgings">
-                    <Button >My stays</Button>
-                </Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Link to="/locations">
-                    <Button>Popular Places</Button>
-                </Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Button onClick={handleLogout}>Logout</Button>
-            </Nav.Item>
-           Welcome {user.username}!
-        </Nav> :
+    {user && user.id ?
+    <Navbar className='nav-font' >
+        <Container>
+          <Link to='/' style={{textDecoration: "none"}}>
+            <Navbar.Brand >WeStay</Navbar.Brand>
+          </Link>
+            <Nav.Link as={Link} to='/locations' >Location List</Nav.Link>
+            <Nav.Link as={Link} to='/lodgings' >Lodging List</Nav.Link>
+            
+          <Nav className="ml-auto">
+          <Navbar.Brand >
+            Welcome: {user.username}
+          </Navbar.Brand>
+          <Dropdown >
+            <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+            </Dropdown.Toggle>
+              
 
-        <Nav className="justify-content-center" activeKey="/">
-            <Nav.Item>
-                <Link to="/">
-                    <Button>Home</Button>
-                </Link>
-            </Nav.Item>
-            
-            <Nav.Item>
-                <Link to="/signup">
-                    <Button>Signup</Button>
-                </Link>
-            </Nav.Item>
-            
-            <Nav.Item>
-                <Link to="/login">
-                    <Button>Login</Button>
-                </Link>
-            </Nav.Item>
-        </Nav>
-        }
+            <Dropdown.Menu className='dropdown' >
+              <Dropdown.Item className='button' onClick={() => setShowModal(true)}>Logout</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          </Nav>
+
+          <Modal
+          show={showModal}
+          onHide={() => setShowModal(false)}
+          backdrop="static"
+          keyboard={false}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>Logout?</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              Are you sure you want to logout?
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={() => setShowModal(false)}>
+                Go back
+              </Button>
+              <Button variant="danger" onClick={handleLogout}>Logout</Button>
+            </Modal.Footer>
+          </Modal>
+        </Container>
+      </Navbar>
+    
+    : 
+    <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Link to='/' style={{textDecoration: "none"}}>
+            <Navbar.Brand >WeStay</Navbar.Brand>
+          </Link>
+          <Nav className="me-auto">
+              <Nav.Link as={Link} to='/signup' >Signup</Nav.Link>
+              <Nav.Link as={Link} to='/login' >Login</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+     
+    }
     </>
     )
 }
